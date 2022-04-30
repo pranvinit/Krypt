47b25856de42a42894214135f7657058cf2a551c
Pranav <pranvofficial@gmail.com>
2022-04-30 15:39:30 +0530

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+// components imports
+import { Navbar, Footer, Welcome, Transactions, Services } from "./components";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
