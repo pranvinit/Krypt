b151bd06d1835788bbafe56491de5107cbbc4128
Pranav <pranvofficial@gmail.com>
2022-04-30 16:25:26 +0530

    designed responsive navbar and welcome section

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,3 +1,33 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+// components imports
+import { Loader } from "./";
+
 export default function Welcome() {
-  return <div>Welcome</div>;
+  const connectWallet = () => {};
+
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 p-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            Send Crypto <br /> across the world.
+          </h1>
+          <p className="text-left text-white mt-5 font-light md:w-9/12 w-11/12 text-base">
+            Explore the crypto world. Buy and sell cryptocurrencies easily on
+            Krypt.
+          </p>
+          <button
+            type="button"
+            onClick={connectWallet}
+            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+          >
+            <p className="text-white text-base font-semibold">Connect Wallet</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
