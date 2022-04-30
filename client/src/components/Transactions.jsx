7abfb12e209da5bc91c6a9cede6bb398cb94eb0b
Pranav <pranvofficial@gmail.com>
2022-04-30 20:56:32 +0530

    added transaction card

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -7,6 +7,9 @@ import { TransactionContext } from "../context/TransactionContext";
 // utils imports
 import { shortenAddress } from "../utils/shortenAddress";
 
+// hooks imports
+import useFetch from "../hooks/useFetch";
+
 const TransactionCard = ({
   addressTo,
   addressFrom,
@@ -16,6 +19,8 @@ const TransactionCard = ({
   amount,
   url,
 }) => {
+  const gifUrl = useFetch({ keyword });
+
   return (
     <div
       className="bg-[#181918] m-4 flex flex-1
@@ -53,10 +58,14 @@ const TransactionCard = ({
               <p className="text-white text-base">Message: {message}</p>
             </>
           )}
-
-          <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-            <p className="text-[#37c7da] font-bold">{timestamp}</p>
-          </div>
+        </div>
+        <img
+          src={gifUrl || url}
+          alt="gif"
+          className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
+        />
+        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+          <p className="text-[#37c7da] font-bold">{timestamp}</p>
         </div>
       </div>
     </div>
---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const API_KEY = import.meta.env.VITE_GIPHY_API;
+
+const useFetch = ({ keyword }) => {
+  const [gifUrl, setGifUrl] = useState("");
+
+  const fetchGifs = async () => {
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/stickers/search?api_key=${API_KEY}&q=${keyword
+          .split(" ")
+          .join("")}&limit=1`
+      );
+
+      const data = await response.json();
+      setGifUrl(data[0]?.images?.downsized_medium?.url);
+    } catch (e) {
+      console.error(e);
+      setGifUrl(
+        "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284"
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchGifs;
+  }, [keyword]);
+
+  return gifUrl;
+};
+
+export default useFetch;
