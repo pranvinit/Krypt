182d0fae82f80bbbc160ca5bbe5c71983594c260
Pranav <pranvofficial@gmail.com>
2022-04-30 17:19:15 +0530

    designed welcome form

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,3 +1,7 @@
 export default function Loader() {
-  return <div>Loader</div>;
+  return (
+    <div className="flex justify-center items-center py-3">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700" />
+    </div>
+  );
 }
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,13 +5,31 @@ import { BsInfoCircle } from "react-icons/bs";
 // components imports
 import { Loader } from "./";
 
+const companyCommonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+const Input = ({ placeholder, name, type, value, handleChange }) => {
+  return (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      vlaue={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+};
+
 export default function Welcome() {
   const connectWallet = () => {};
 
+  const handleSubmit = () => {};
+
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 p-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 p-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world.
           </h1>
@@ -26,6 +44,80 @@ export default function Welcome() {
           >
             <p className="text-white text-base font-semibold">Connect Wallet</p>
           </button>
+
+          <div className="grid sm:grid-cols-3 gird-cols-3 w-full mt-10">
+            <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+              Reliability
+            </div>
+            <div className={companyCommonStyles}>Security</div>
+            <div className={`rounded-tr-2xl ${companyCommonStyles}`}>
+              SiEthereum
+            </div>
+            <div className={`rounded-bl-2xl ${companyCommonStyles}`}>
+              WEB 3.0
+            </div>
+            <div className={companyCommonStyles}>Low fees</div>
+            <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+              Blockchain
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1 ">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+            <Input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              onChange={() => {}}
+            />
+            <Input
+              placeholder="Amount (ETH)"
+              name="amount"
+              type="number"
+              onChange={() => {}}
+            />
+            <Input
+              placeholder="Keyword (Gif)"
+              name="keyword"
+              type="text"
+              onChange={() => {}}
+            />
+            <Input
+              placeholder="Enter Message"
+              name="message"
+              type="text"
+              onChange={() => {}}
+            />
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
+            {true ? (
+              <Loader />
+            ) : (
+              <button
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                type="button"
+                onClick={handleSubmit}
+              >
+                Send Now
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
